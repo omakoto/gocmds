d1d1f0906c9e1416811e40ae1978c66a105281c6
Makoto Onuki <omakoto@google.com>
2018-05-04 20:43:18 -0700

    native shescape

---
+++ cmd/shescapecommon/shescape.go
@@ -0,0 +1,25 @@
+package shescapecommon
+
+import (
+	"fmt"
+	"github.com/omakoto/go-common/src/common"
+	"github.com/omakoto/go-common/src/shell"
+	"github.com/omakoto/go-common/src/textio"
+)
+
+func ShescapeNoNewline(args []string) {
+	for i, arg := range args {
+		if i > 0 {
+			fmt.Print(" ")
+		}
+		fmt.Print(shell.Escape(arg))
+	}
+}
+
+func ShescapeStdin(files []string) {
+	err := textio.ReadFiles(files, func(line []byte, lineNo int, filename string) error {
+		fmt.Print(string(shell.EscapeBytes(textio.Chomp(line))), "\n")
+		return nil
+	})
+	common.Check(err, "Unable to read file")
+}
---
+++ cmd/shescapes/shescapes.go
@@ -0,0 +1,17 @@
+///bin/true; exec /usr/bin/env go run "$0" "$@"
+package main
+
+import (
+	"github.com/omakoto/go-common/src/common"
+	"github.com/omakoto/gocmds/cmd/shescapecommon"
+	"os"
+)
+
+func main() {
+	common.RunAndExit(realMain)
+}
+
+func realMain() int {
+	shescapecommon.ShescapeStdin(os.Args)
+	return 0
+}
