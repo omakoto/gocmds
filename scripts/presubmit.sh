127dc4ca55935266bffcf5b0adba34c4b88356e5
Makoto Onuki <makoto.onuki@google.com>
2018-08-28 20:11:30 -0700

    Update for go 1.11

--- scripts/presubmit.sh
+++ scripts/presubmit.sh
@@ -11,4 +11,4 @@ go test -v -race ./...                   # Run all the tests with the race detec
 echo "Running extra checks..."
 go vet ./...                             # go vet is the official Go static analyzer
 megacheck ./...                          # "go vet on steroids" + linter
-golint $(go list ./...) |& grep -v 'exported .* should have' | perl -pe 'END{exit($. > 0 ? 1 : 0)}'
+golint $(go list ./... | grep -v 'oauther') |& grep -v 'exported .* should have' | perl -pe 'END{exit($. > 0 ? 1 : 0)}'
