090b93102d170380c1b6d40a07af276d249414c6
Makoto Onuki <makoto.onuki@google.com>
2024-12-31 18:15:01 -0800

    latest

--- src/cmd/multiplex/multiplex.go
+++ src/cmd/multiplex/multiplex.go
@@ -7,12 +7,20 @@ import (
 	"fmt"
 	"os"
 	"sync"
+	"sync/atomic"
 )
 
 func main() {
 	files := os.Args[1:]
 	var wg sync.WaitGroup
 	var mu sync.Mutex
+
+	var numSuccess atomic.Int32
+
+	if len(files) == 0 {
+		fmt.Fprintln(os.Stderr, "Usage: multiplex <file1> <file2> ...")
+		os.Exit(3)
+	}
 
 	for _, file := range files {
 		wg.Add(1)
@@ -36,9 +44,19 @@ func main() {
 
 			if err := scanner.Err(); err != nil {
 				fmt.Fprintf(os.Stderr, "Error reading file %s: %v\n", file, err)
+				return
 			}
+			numSuccess.Add(1)
 		}(file)
 	}
-
 	wg.Wait()
+
+	var ns = int(numSuccess.Load())
+	status := 0
+	if ns == 0 {
+		status = 2
+	} else if ns < len(files) {
+		status = 1
+	}
+	os.Exit(status)
 }
